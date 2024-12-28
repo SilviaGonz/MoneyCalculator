6db89067453d8fb2068fcca1e5c90d539ce009bd
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-12-28 11:55:14 +0000

    class Main

---
+++ src/main/java/software/moneycalculator/Main.java
@@ -0,0 +1,10 @@
+package software.moneycalculator;
+
+import software.moneycalculator.swing.SwingMainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingMainFrame swingMainFrame = new SwingMainFrame();
+        swingMainFrame.setVisible(true);
+    }
+}
